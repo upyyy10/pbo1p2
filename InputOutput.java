11607516811e373842cf0216935e9af0a1e037c5
upyyy10 <diskursifdandapati@gmail.com>
2021-04-04 21:04:55 +0800

    input output

---
+++ InputOutput.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class InputOutput{
+ public static void main(String [] args){ 
+  Scanner scanner = new Scanner(System.in);
+  String nama;
+  int jumlahSks;
+  double IPK;
+
+  System.out.print("Masukkan nama : ");
+  nama = scanner.nextLine();
+  System.out.println("Hai, "+nama);
+
+  System.out.print("Masukkan jumlah SKS yang sudah ditempuh : ");
+  jumlahSks = scanner.nextInt();
+
+  System.out.print("Masukkan IPK Sementara : ");
+  IPK = scanner.nextDouble();
+
+  double jumlah = IPK * jumlahSks;
+  System.out.print("Total Nilai = "+jumlah);
+
+ }
+}
